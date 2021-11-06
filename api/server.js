a158d0f5ca2bdac96ad48e9eaf14ae711732f583
stkirk <shaanekirk@gmail.com>
2021-11-06 11:33:13 -0400

    added error handling middleware, catchall endpoint, and home endpoint

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const restrict = require('./middleware/restricted.js');
+const restrict = require("./middleware/restricted.js");
 
-const authRouter = require('./auth/auth-router.js');
-const jokesRouter = require('./jokes/jokes-router.js');
+const authRouter = require("./auth/auth-router.js");
+const jokesRouter = require("./jokes/jokes-router.js");
 
 const server = express();
 
@@ -13,7 +13,22 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", restrict, jokesRouter); // only logged-in users should have access!
+
+server.get("/", (req, res) => {
+  res.json({ message: "api up" });
+});
+
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "not found" });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
